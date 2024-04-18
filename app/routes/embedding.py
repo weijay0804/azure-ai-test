3275078de5f5f64804cbb6cc5ebf5ad811787490
weijay <weijay0804@gmail.com>
2024-04-18 21:59:28 +0800

    feat: 新增 將使用者上傳的 txt 文件資訊儲存至資料庫功能

--- app/routes/embedding.py
+++ app/routes/embedding.py
@@ -5,10 +5,12 @@
 import uuid
 
 from fastapi import APIRouter, UploadFile, HTTPException, Depends
+from sqlalchemy.orm import Session
 from qdrant_client import QdrantClient
 
 from app.services import embedding
-from app.config.database import get_qdrant_session
+from app.models.embedding import EmbeddingFile
+from app.config.database import get_qdrant_session, get_db_session
 
 embedding_router = APIRouter(
     prefix="/embedding", tags=["Embedding"], responses={404: {"description": "Not found."}}
@@ -17,7 +19,9 @@ embedding_router = APIRouter(
 
 @embedding_router.post("/file")
 def upload_embedding_txt_file(
-    file: UploadFile, qsession: QdrantClient = Depends(get_qdrant_session)
+    file: UploadFile,
+    qsession: QdrantClient = Depends(get_qdrant_session),
+    db_session: Session = Depends(get_db_session),
 ):
     """上傳 txt 檔案並進行 embedding"""
 
@@ -38,4 +42,10 @@ def upload_embedding_txt_file(
     # 上傳至 Azure Blob
     url = embedding.upload_to_azure_blob(file, f"{uid}.txt")
 
-    return {"message": "file upload success.", "azure_url": url}
+    # TODO 這邊要重構一下，不要把操作資料庫的動作寫在 router
+    embedding_file_obj = EmbeddingFile(id=uid, raw_filename=file.filename, azure_blob_url=url)
+
+    db_session.add(embedding_file_obj)
+    db_session.commit()
+
+    return {"message": "file upload success.", "azure_url": url, "id": uid}
